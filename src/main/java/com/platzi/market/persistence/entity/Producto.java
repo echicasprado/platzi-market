db21ca84f041a80fc9863ccfdec1daf0bf75c48e
everchicas <everchicasp@outllook.com>
2024-04-10 16:08:19 -0600

    Add entities

---
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name="codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_veta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer catidadStock;
+
+    private boolean estado
+
+    public Integert getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integert idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCatidadStock() {
+        return catidadStock;
+    }
+
+    public void setCatidadStock(Integer catidadStock) {
+        this.catidadStock = catidadStock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
